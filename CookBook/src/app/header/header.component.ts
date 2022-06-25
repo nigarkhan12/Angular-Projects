faf7e737fa488ec1739dd92d24b84abda3681898
nigar khan <nigarkhan92@gmail.com>
2022-06-25 14:26:26 +0530

    added navigation with Event Binding & ngIf

--- CookBook/src/app/header/header.component.ts
+++ CookBook/src/app/header/header.component.ts
@@ -1,4 +1,8 @@
-import { Component } from "@angular/core";
+import {
+    Component,
+    EventEmitter,
+    Output
+} from "@angular/core";
 
 @Component({
     'selector':'app-header',
@@ -7,5 +11,11 @@ import { Component } from "@angular/core";
 })
 
 export class HeaderComponent{
-collapsed=true;
+    collapsed = true;
+    
+    @Output() featureSelected = new EventEmitter<string>();
+    
+    onSelect(feature: string) {
+        this.featureSelected.emit(feature);
+    }
 }
