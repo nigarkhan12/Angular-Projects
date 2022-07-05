1aeb1c22753b41eda8fae7d1cdd4c69bc5fd0a8f
nigar khan <nigarkhan92@gmail.com>
2022-07-05 18:03:46 +0530

    added ingredients to recipe

--- CookBook/src/app/recipes/recipe.service.ts
+++ CookBook/src/app/recipes/recipe.service.ts
@@ -1,12 +1,27 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService{
     recipeSelected = new EventEmitter<Recipe>();
     
     private recipes:Recipe[] = [
-        new Recipe('Pasta', "This is Pasta recipe", "https://picturetherecipe.com/wp-content/uploads/2020/04/Vanilla-Cardamom-Kulfi-by-PictureTheRecipe.jpg"),
-        new Recipe('Pizza', "This is Pizza recipe", "https://picturetherecipe.com/wp-content/uploads/2020/04/Vanilla-Cardamom-Kulfi-PTR.jpg"),
+        new Recipe('Pasta',
+            "This is Pasta recipe",
+            "https://picturetherecipe.com/wp-content/uploads/2020/04/Vanilla-Cardamom-Kulfi-by-PictureTheRecipe.jpg",
+            [
+                new Ingredient('Meat',1),
+                new Ingredient('French Fries',20 ),
+            ]
+        ),
+        new Recipe('Pizza',
+            "This is Pizza recipe",
+            "https://picturetherecipe.com/wp-content/uploads/2020/04/Vanilla-Cardamom-Kulfi-PTR.jpg",
+            [
+                new Ingredient('chicken',1),
+                new Ingredient('cheese',20 ),
+            ]
+        ),
     ];
     
     getRecipes() {
